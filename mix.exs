ae9dc4404de1e762c55bd5b07066f9a73ce871fa
Bart Blast <bart@bartblast.com>
2025-03-10 14:45:37 +0100

    Do not setup Ecto repo in mix setup task

--- mix.exs
+++ mix.exs
@@ -71,7 +71,8 @@ defmodule HologramSkeleton.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      # setup: ["deps.get", "ecto.setup", "assets.setup", "assets.build"],
+      setup: ["deps.get", "assets.setup", "assets.build"],
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
